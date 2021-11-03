0c93a57d39fdd98937ac41f66e8d5b4eb19bbc66
Mauricio Barva <mauriciobarva@gmail.com>
2021-11-03 17:15:20 -0500

    vista del login creada

--- practicas/urls.py
+++ practicas/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from learning import views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.login),
 ]
