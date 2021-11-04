7c9ad8f5802c7028333ef7dd8ecc428476493f1e
Mauricio Barva <mauriciobarva@gmail.com>
2021-11-04 17:04:33 -0500

    setup inicial allauth añadidos

--- practicas/urls.py
+++ practicas/urls.py
@@ -13,6 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import include
 from django.contrib import admin
 from django.urls import path
 from learning import views
@@ -20,6 +21,7 @@ from learning import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('allauth.urls')),
     path('', views.login),
    
 ]
