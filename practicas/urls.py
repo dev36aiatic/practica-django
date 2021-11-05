ee83c0f48b197f3e19e894e7e0de943e26da6a8f
Mauricio Barva <mauriciobarva@gmail.com>
2021-11-05 12:15:27 -0500

    autenticación con google y facebook añadida

--- practicas/urls.py
+++ practicas/urls.py
@@ -13,15 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf.urls import include
 from django.contrib import admin
 from django.urls import path
+from django.urls.conf import include
 from learning import views
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/', include('allauth.urls')),
+     path('accounts/', include('allauth.urls')),
     path('', views.login),
    
 ]
