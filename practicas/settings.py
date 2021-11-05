756ac2b90a123fa9ea8a9757bd9a9f44d504f8d5
Mauricio Barva <mauriciobarva@gmail.com>
2021-11-05 17:05:12 -0500

    push prev heroku

--- practicas/settings.py
+++ practicas/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
     'django.contrib.sites',
     'django.contrib.staticfiles',
     'learning',
+    'templates',
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
